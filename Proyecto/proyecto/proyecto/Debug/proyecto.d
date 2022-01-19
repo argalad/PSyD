a09176a82763c9cfe606d527d7b655d0f59834f5
Alberto R.R. Manzanares <17321519+AlbertoRRM@users.noreply.github.com>
2022-01-19 10:57:04 +0100

    Proyecto Game & Watch

---
+++ Proyecto/proyecto/proyecto/Debug/proyecto.d
@@ -0,0 +1,25 @@
+proyecto.d proyecto.o: ../proyecto.c ../../BSP/include/s3c44b0x.h \
+ ../../BSP/include/common_types.h ../../BSP/include/s3cev40.h \
+ ../../BSP/include/system.h ../../BSP/include/timers.h \
+ ../../BSP/include/lcd.h ../../BSP/include/pbs.h ../../BSP/include/iis.h \
+ ../../BSP/include/keypad.h ../../BSP/include/rtc.h
+
+../../BSP/include/s3c44b0x.h:
+
+../../BSP/include/common_types.h:
+
+../../BSP/include/s3cev40.h:
+
+../../BSP/include/system.h:
+
+../../BSP/include/timers.h:
+
+../../BSP/include/lcd.h:
+
+../../BSP/include/pbs.h:
+
+../../BSP/include/iis.h:
+
+../../BSP/include/keypad.h:
+
+../../BSP/include/rtc.h:
