903d485ba5a7103a25dc77c1ce93c3f5d47c0ecb
Argalad <17321519+argalad@users.noreply.github.com>
2023-07-06 19:13:26 +0200

    Delete labs directory

--- labs/lab3/Debug/systemLab3.d
+++
@@ -1,8 +0,0 @@
-systemLab3.d systemLab3.o: ../systemLab3.c ../../BSP/include/s3c44b0x.h \
- ../../BSP/include/common_types.h ../systemLab3.h
-
-../../BSP/include/s3c44b0x.h:
-
-../../BSP/include/common_types.h:
-
-../systemLab3.h:
